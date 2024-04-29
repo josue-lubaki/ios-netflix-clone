4328aeae8d45134f1f8f99888295da1007081b45
Josue Lubaki <josuelubaki@gmail.com>
2024-04-29 14:28:54 -0400

    Initial Commit

---
+++ netflix/netflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  netflixApp.swift
+//  netflix
+//
+//  Created by Josue Lubaki on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct netflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
