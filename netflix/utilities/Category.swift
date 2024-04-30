e5c236e8004ab8ef219870d97cdacd723fec2471
Josue Lubaki <josuelubaki@gmail.com>
2024-04-30 17:09:09 -0400

    Animate Heade Scrollr

---
+++ netflix/utilities/Category.swift
@@ -0,0 +1,12 @@
+//
+//  Category.swift
+//  netflix
+//
+//  Created by Josue Lubaki on 2024-04-30.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case all, music, podcasts, audiobooks
+}
---
+++ netflix/utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  netflix
+//
+//  Created by Josue Lubaki on 2024-04-30.
+//
+
+import Foundation
+
+struct Constants {
+    static let randomImage = "https://picsum.photos/600/600"
+}
